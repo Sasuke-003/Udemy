10de6ec634cf56654a211bf4d24233a3f8306138
Sasuke-003 <56964273+Sasuke-003@users.noreply.github.com>
2020-04-24 15:47:31 +0530

    Updates

---
+++ precache-manifest.9295e55573d6665e23d7dfff2a880922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bd50a922db1ea4515a7db29a46607c",
+    "url": "/Udemy/React/my-app-1/index.html"
+  },
+  {
+    "revision": "430f530043988a3e4b85",
+    "url": "/Udemy/React/my-app-1/static/css/main.20028231.chunk.css"
+  },
+  {
+    "revision": "22d8f03f3cb121289cf7",
+    "url": "/Udemy/React/my-app-1/static/js/2.4ecaa5e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Udemy/React/my-app-1/static/js/2.4ecaa5e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430f530043988a3e4b85",
+    "url": "/Udemy/React/my-app-1/static/js/main.606c779f.chunk.js"
+  },
+  {
+    "revision": "edced9fde70155ee1bef",
+    "url": "/Udemy/React/my-app-1/static/js/runtime-main.c8793ed5.js"
+  }
+]);
